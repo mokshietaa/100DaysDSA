84041038da8568868e72b940762b55759ff6c6c7
Mokshieta Ragta <mokshietaa@gmail.com>
2026-04-12 12:46:01 +0530

    Day49

---
+++ bst.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *left, *right;
+} Node;
+
+Node *newNode(int val)
+{
+    Node *temp = (Node *)malloc(sizeof(Node));
+    temp->data = val;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+Node *insert(Node *root, int key)
+{
+    if (root == NULL)
+        return newNode(key);
+
+    if (key < root->data)
+        root->left = insert(root->left, key);
+    else if (key > root->data)
+        root->right = insert(root->right, key);
+
+    return root;
+}
+
+void inorder(Node *root)
+{
+    if (!root)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main()
+{
+    int n, x;
+    scanf("%d", &n);
+
+    Node *root = NULL;
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    inorder(root);
+
+    return 0;
+}
