9e948d8bd50cd450eeda469338fa9134fffcbd0c
Mokshieta Ragta <mokshietaa@gmail.com>
2026-05-03 22:49:11 +0530

    Add files via upload
    Day51

---
+++ q51.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* LCA(struct Node* root, int n1, int n2) {
+    while(root != NULL) {
+        if(n1 < root->data && n2 < root->data)
+            root = root->left;
+        else if(n1 > root->data && n2 > root->data)
+            root = root->right;
+        else
+            return root;
+    }
+    return NULL;
+}
